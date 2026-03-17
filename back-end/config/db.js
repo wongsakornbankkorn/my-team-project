d6dcb9db8c6daa9dd3c0a7039b1b7b87401b5301
wongsakornbankkorn <wongsakorn.bankkorn@gmail.com>
2026-03-17 15:46:45 +0700

    ิbaen

---
+++ back-end/config/db.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require('sequelize')
+require('dotenv').config()
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  }
+)
+
+sequelize.authenticate()
+  .then(() => console.log('Database connected'))
+  .catch(err => console.error('Connection error:', err))
+
+module.exports = sequelize
---
+++ back-end/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+require('./config/db')   
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+ 
+
+app.use('/api/items',      require('./routes/itemRoutes'))
+app.use('/api/categories', require('./routes/categoryRoutes'))
+app.use('/api/users',      require('./routes/userRoutes'))
+app.use('/api/auth',       require('./routes/authRoutes'))
+app.use('/api/locations',  require('./routes/locationRoutes'))
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
